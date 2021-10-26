551e497321f0ed0fa5ea3a05167a4b5c976c80f6
kakxow <kakxow@gmail.com>
2021-10-26 17:02:18 +0300

    something works! WIP

--- game.py
+++ game.py
@@ -1,8 +1,9 @@
-from cards import cards, Card
+from typing import Generator
+from cards import cards, Card, colors
 from stuff import Deck, Player
 
 
-player_number = 4
+player_number = 2
 
 
 deck = Deck(cards)
@@ -11,39 +12,67 @@ players: list[Player] = []
 
 class Game:
     def __init__(self):
-        pass
+        self.number_of_players = player_number
+        self.current_player_id = 0
+        self.player_increment = 1
 
     def get_next_player(self) -> Player:
-        pass
+        self.current_player_id += self.player_increment
+        if self.current_player_id > self.number_of_players - 1:
+            self.current_player_id = 0
+        elif self.current_player_id < 0:
+            self.current_player_id = self.number_of_players - 1
+        return self.players[self.current_player_id]
 
     def reverse_players(self):
-        pass
+        self.player_increment *= -1
 
-    def start_session(self):
-        """ Start a session of several games. """
-        pass
-
-    def play_card(self, card: Card):
+    def card_action(self, card: Card):
         getattr(self, card.action)()
 
     def reverse(self):
         self.reverse_players()
+        # self.get_next_player()  # cuz next player is picked before the card action plays.
 
     def skip(self):
-        self.get_next_player()
+        pass
+        # self.get_next_player()
 
     def draw_two(self):
-        self.get_next_player().draw(2)
+        self.current_player.draw(2)
 
-    def start_game(self, ):
+    def wild(self):
+        pass
+
+    def wild_draw_four(self):
+        self.current_player.draw(4)
+    
+    def start_game(self):
         """ Start a game (one of many!) during this playing session. """
         deck.shuffle()
-        self.players = [Player(str(i), [], deck) for i in range(1, player_number + 1)]  # temp
-        current_player = self.get_next_player()
-        last_card = current_player.play()
+        self.players: list[Player] = []
+        for i in range(player_number):
+            self.players.append(Player(str(i), [], deck))
+        # breakpoint()
+        print(*self.players, sep="\n")
+        
+        # self.players = [Player(str(i), [], deck) for i in range(1, player_number + 1)]  # temp
+        self.current_player = self.get_next_player()
+        self.last_card = self.current_player.drop()
+        breakpoint()
         while True:
-            current_player = self.get_next_player()
-            if last_card.action:
-                self.play_card(last_card)
+            self.current_player = self.get_next_player()
+            if self.last_card.action and self.last_card.is_active:
+                self.card_action(self.last_card)
+                self.last_card.is_active = False
+                print(self.last_card)
             else:
-                pass
+                self.last_card = self.current_player.turn(self.last_card)
+                if not self.current_player.hand:
+                    print(f"{self.current_player.name} won!")
+                    break
+        # self.score()
+
+game = Game()
+game.start_game()
+
--- stuff.py
+++ stuff.py
@@ -1,18 +1,22 @@
 from dataclasses import dataclass
 import random
+from typing import MutableSequence
 
-from cards import Card, special_colors
+from cards import Card, colors
 from game_setup import starting_hand_size
 
 
-class Deck(list):
+class Deck:
+    def __init__(self, cards: list[Card]) -> None:
+        self.cards = list(cards)
+
     def draw(self, x: int = 1):
-        res = self[:x]
-        self = self[x:]  # type: ignore
+        res = self.cards[:x]
+        self.cards = self.cards[x:]
         return res
 
     def shuffle(self):
-        random.shuffle(self)
+        random.shuffle(self.cards)
 
 
 @dataclass()
@@ -29,19 +33,66 @@ class Player:
         self.draw(starting_hand_size)
 
     def is_eligible_for_play(self, card_in_hand: Card, card_on_table: Card) -> bool:
-        return (card_in_hand.color == card_on_table.color or
-                card_in_hand.value == card_on_table.value or
-                card_in_hand.action == card_on_table.action or
-                card_in_hand.color in special_colors)
+        return (card_in_hand.color == card_on_table.color or  # same color
+                card_in_hand.value == card_on_table.value and card_in_hand.value is not None or  # or same value
+                card_in_hand.action == card_on_table.action and card_in_hand.action != "")  # or same action
 
     def check_hand(self, last_played_card: Card) -> list[Card]:
-        return [card for card in self.hand if self.is_eligible_for_play(card, last_played_card)]
+        regular_cards: list[Card] = []
+        wild: list[Card] = []
+        wild4: list[Card] = []
+        for card in self.hand:
+            if self.is_eligible_for_play(card, last_played_card):
+                regular_cards.append(card)
+            else:
+                match card.action:
+                    case "wild": wild.append(card)
+                    case "wild_draw_four": wild4.append(card)
+                    case _: pass
+        return regular_cards or wild or wild4
 
-    def is_win(self, play_choices: list[Card]) -> bool:
-        return len(play_choices) == 1 and len(self.hand) == 2
+    def drop(self) -> Card:
+        card = self.hand.pop(random.randint(0, len(self.hand)-1))
+        if card.color == "black":
+            color = ""
+            while color not in colors:
+                color = input("Select a new color - red, blue, yellow, green")
+            card.color = color
+        print(card)
+        return card
 
-    def play(self):
-        return self.hand.pop(random.randint(0, len(self.hand) + 1))
+    def play(self, play_choices: list[Card]) -> Card:
+        #for i, card in enumerate(self.hand):
+        #    print(i, card)
+        print(*enumerate(play_choices), sep="\n")
+        while True:
+            card_id_raw = input("Select card id to play.")
+            if card_id_raw.isdecimal():
+                card_id = int(card_id_raw)
+                if 0 <= card_id <= len(play_choices):
+                    break
+        played_card = play_choices[card_id]
+        self.hand.remove(played_card)
+        return played_card
 
-    def draw(self, x: int):
+    def turn(self, last_card: Card) -> Card:
+        """ play a card if can """
+        print(self)
+        play_choices = self.check_hand(last_card)
+        if play_choices:
+            
+            played_card = self.play(play_choices)
+            if played_card.color == "black":
+                color = ""
+                while color not in colors:
+                    color = input("Select a new color - red, blue, yellow, green")
+                played_card.color = color
+            print(played_card)
+            return played_card
+        else:
+            print("draws a card")
+            self.draw()
+            return last_card
+
+    def draw(self, x: int = 1):
         self.hand += self.deck.draw(x)
